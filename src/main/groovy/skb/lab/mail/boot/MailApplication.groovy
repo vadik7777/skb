e263dfb00dda07c3e4a9f1ce5eaddb0af3ca5399
79041 <123>
2019-12-14 23:32:31 +0500

    First Commit

---
+++ src/main/groovy/skb/lab/mail/boot/MailApplication.groovy
@@ -0,0 +1,13 @@
+package skb.lab.mail.boot
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MailApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MailApplication, args)
+	}
+
+}
