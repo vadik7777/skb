81b715bf29da3ea7a69027144c66afffd569163c
79041 <123>
2019-12-16 00:54:00 +0500

    Add JSP config

---
+++ src/main/groovy/skb/lab/mail/controller/Welocme.groovy
@@ -0,0 +1,16 @@
+package skb.lab.mail.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+
+@Controller
+public class Welcome {
+
+    @GetMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+
+}
